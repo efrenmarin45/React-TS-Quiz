61ab7b2f5e24b7f729b88ad32309c4787bed4ef5
Efren_Marin <efren45marin@gmail.com>
2021-08-01 04:45:17 -0700

    Updated backgrounds and media queries

--- src/styles.ts
+++ src/styles.ts
@@ -1,6 +1,6 @@
 import styled, { createGlobalStyle } from "styled-components";
 import BGImage from "./img/BGImage1.jpg";
-import { device } from "./device";
+import BGImageSm from "./img/BGImage9.jpg";
 
 export const GlobalStyle = createGlobalStyle`
     html{
@@ -21,16 +21,16 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Catamaran', sans-serif;
     }
 
-	@media (max-width: 980px){
+	@media (max-width: 1025px){
 		body{
 			background-image: none;
 			background: rgb(125,254,255);
 			background: linear-gradient(145deg, rgba(125,254,255,1) 0%, rgba(219,83,225,1) 100%);
 		}
-	}
-
-	@media ${device.laptop}{
-		max-width: 800px;
+		body{
+			background-image: url(${BGImageSm});
+			background-size: contain;
+		}
 	}
 `;
 
@@ -59,7 +59,6 @@ export const Wrapper = styled.div`
 		font-family: "Staatliches", sans serif;
 		background-color: #fff;
 		border-radius: 20px;
-		background-size: 100%;
 		padding: 5px 25px;
 		filter: drop-shadow(4px 2px #3ba0f2);
 		font-size: 70px;
@@ -84,6 +83,28 @@ export const Wrapper = styled.div`
 	.start {
 		max-width: 200px;
 		font-size: 1.3rem;
+		-webkit-animation-name: start; /* Safari 4.0 - 8.0 */
+		-webkit-animation-duration: 4s; /* Safari 4.0 - 8.0 */
+		animation-name: start;
+		animation-duration: 1s;
+		animation-iteration-count: infinite;
+	}
+
+	/* Safari 4.0 - 8.0 */
+	@-webkit-keyframes start {
+		0%   {background-color: #00a0cc;}
+		15%  {background-color: #00B0E0;}
+		25%  {background-color: #00C0F6;}
+		100% {background-color: #00a0cc;}
+	}
+	
+
+	/* Standard syntax */
+	@keyframes start {
+		0%   {background-color: #00a0cc;}
+		15%  {background-color: #00B0E0;}
+		25%  {background-color: #00C0F6;}
+		100% {background-color: #00a0cc;}
 	}
 
 	.loadingText {
@@ -93,5 +114,73 @@ export const Wrapper = styled.div`
 		text-shadow: 2px 2px black;
 	}
 
+	@media (max-width: 1025px){
+		h1{
+			font-size: 60px;
+			filter: drop-shadow(4px 2px #038951);
+		}
+
+		.start{
+			background-color: #038951;
+		}
+
+		/* Safari 4.0 - 8.0 */
+		@-webkit-keyframes start {
+			0%   {background-color: #00693E;}
+			25%  {background-color: #177245;}
+			50%  {background-color: #03C03C;}
+			100% {background-color: #00693E;}
+		}
+		
+
+		/* Standard syntax */
+		@keyframes start {
+			0%   {background-color: #00693E;}
+			25%  {background-color: #177245;}
+			50%  {background-color: #03C03C;}
+			100% {background-color: #00693E;}
+		}
+	}
+
+	@media (max-width: 800px){
+		h1{
+			font-size: 50px;
+		}
+	}
+
+	@media (max-width: 450px){
+		h1{
+			font-size: 40px;
+		}
+
+		.start{
+			max-width: 175px;
+			font-size: 1.2rem;
+			padding: 0 35px;
+			background-color: #038951;
+		}
+	}
+
+	@media (max-width: 380px){
+		h1{
+			font-size: 30px;
+		}
+
+		.start{
+			max-width: 150px;
+			font-size: 1.1rem;
+		}
+	}
+
+	@media (max-width: 300px){
+		h1{
+			font-size: 25px;
+		}
+
+		.start{
+			max-width: 125px;
+			font-size: 1rem;
+		}
+	}
 	
 `;
